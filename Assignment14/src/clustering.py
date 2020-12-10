026e184e017e1e01195cc85648321e8502de2e1b
jihunJeong <jihunJeong@gmail.com>
2020-12-10 13:16:20 +0900

    upload Document

--- Assignment14/src/clustering.py
+++ Assignment14/src/clustering.py
@@ -28,7 +28,7 @@ def mean_shift(img):
     rst_img = np.zeros((96, 128, 3), dtype=np.float32)
     
     print(img.shape)
-    clustering = MeanShift(bandwidth=10).fit(img)
+    clustering = MeanShift(bandwidth=12.5).fit(img)
     centers = clustering.cluster_centers_
     k = len(centers)
     label = clustering.labels_
@@ -37,20 +37,39 @@ def mean_shift(img):
         rebuild[i] = centers[label[i]]
     
     rst_img = rebuild.reshape((96, 128, 3))
-    lab = cv2.cvtColor(rst_img, cv2.COLOR_LAB2BGR).astype(np.uint8)
+    lab = cv2.cvtColor(rst_img.astype(np.uint8), cv2.COLOR_LAB2BGR)
+    lab = cv2.resize(lab, (0,0), fx=2, fy=2)
     cv2.imshow("Result", lab)
     cv2.waitKey(0)
 
-    cv2.imwrite('../result'+'2'+'.png', lab)
+    cv2.imwrite('../result/'+'mean_shift2'+'.png', lab)
         
     return k
 
 def k_means(img, k):
+    rebuild = np.zeros((96*128, 3), dtype=np.float32)
+    rst_img = np.zeros((96, 128, 3), dtype=np.float32)
     
+    print(img.shape)
     clustering = KMeans(n_clusters=k).fit(img)
+    centers = clustering.cluster_centers_
+    k = len(centers)
+    label = clustering.labels_
+    for i in range(len(label)):
+        rebuild[i] = centers[label[i]]
+    
+    rst_img = rebuild.reshape((96, 128, 3))
+    lab = cv2.cvtColor(rst_img.astype(np.uint8), cv2.COLOR_LAB2BGR)
+    lab = cv2.resize(lab, (0,0), fx=2, fy=2)
+    cv2.imshow("Result", lab)
+    cv2.waitKey(0)
+
+    cv2.imwrite('../result/'+'k_means2'+'.png', lab)
+        
+    return k
 
 if __name__ == "__main__":
     imgs = readImages('../image/test')
     
     k = mean_shift(imgs[1])
-    # k_means(imgs, k)
+    k_means(imgs[1], k)
